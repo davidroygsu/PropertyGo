a162eca8ae93e603e6149b9f983def3493dc6963
davidroygsu <124539273+davidroygsu@users.noreply.github.com>
2023-05-01 15:14:41 -0400

    Update buyerregister.tsx

--- client/src/components/buyerregister.tsx
+++ client/src/components/buyerregister.tsx
@@ -1,39 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
-import { useForm } from "react-hook-form";
-// Import our custom CSS
-import '../scss/styles.scss';
+import { Controller, useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
+import { object, string } from "yup";
+import { TextField, Button } from "@mui/material";
+// Import our custom CSS
+import '../scss/styles.scss';
 
 export default function BuyerRegister() {
-    type Inputs = {
-        username: string;
-        fName: string;
-        lName: string;
-        email: string;
-        password: string;
-    }
 
-    const schema = yup.object().shape({
-        username: yup.string().required(),
-        fName: yup.string().required(),
-        lName: yup.string().required(),
-        email: yup.string().required().email(),
-        password: yup.string().required(),
-    })
+    let userSchema = object({
+        username: string().required('Username is required.'),
+        fName: string().required('First name is required.'),
+        lName: string().required('Last name is required.'),
+        email: string().required('Email is required.').email('Must be a valid email.'),
+        password: string().required('Password is required.'),
+        confirmPassword: string().required('Password confirmation is required.').test(
+            'passwords-match',
+            'Passwords must match',
+            function (value) {
+                return value === this.parent.password;
+            }
+        ),
+        userType: string().required(),
+    });
 
+    const { register, handleSubmit, formState: { errors }, reset, watch, control } = useForm({
+        resolver: yupResolver(userSchema),
+        defaultValues: { username: "", fName: "", lName: "", email: "", password: "", confirmPassword: "", userType: "buyer"}
+    });
     const navigate = useNavigate();
-    const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm();
-
-    // This function will handle the submission.
-    async function onSubmit(e: { preventDefault: () => void; }) {
-        e.preventDefault();
+    async function onSubmit(e: any) {
 
         // When a post request is sent to the create url, we'll add a new record to the database.
-        const newPerson = { ...schema };
+        const newPerson = { ...e };
 
         await fetch("http://localhost:5000/record/add", {
             method: "POST",
@@ -48,111 +50,98 @@ export default function BuyerRegister() {
                 return;
             });
 
-        setForm({ username: "", fName: "", lName: "", email: "", password: "" });
+        reset()
         navigate("/");
     }
 
     return (
-        <html lang="en">
-
+        <>
             <head>
                 <meta charSet="UTF-8" />
                 <title>Wi-Find Buyer | Register </title>
             </head>
-
-            <body className="text-center">
-                <main className="form-signin w-100 m-auto">
-                    <h1 className="h3 mb-3 fw-normal">Wi-Find | Register as a Buyer</h1>
-                    <form onSubmit={onSubmit}>
-
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Username</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="username"
-                                    value={form.username}
-                                    onChange={(e) => updateForm({ username: e.target.value })} />
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">First Name</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="fName"
-                                    value={form.fName}
-                                    onChange={(e) => updateForm({ fName: e.target.value })} />
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Last Name</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="lName"
-                                    value={form.lName}
-                                    onChange={(e) => updateForm({ lName: e.target.value })} />
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Email</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="email"
-                                    value={form.email}
-                                    onChange={(e) => updateForm({ email: e.target.value })} />
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Password</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="password"
-                                    value={form.password}
-                                    onChange={(e) => updateForm({ password: e.target.value })} />
-                            </div>
-                        </div>
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Confirm Password</label>
-                            <div className="col-sm-8">
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="fName"
-                                    value={form.password}
-                                    onChange={(e) => updateForm({ password: e.target.value })} />
-                            </div>
-                        </div>
-
-
-
-                        <div className="row mb-3">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                        <div className="row">
-                            <a href="/buyerlogin"> Already registered?</a>
-
-                        </div>
-
+            <body className="text-center calm">
+                <div>
+                    <h1>Buyer Registration</h1>
+                    <form onSubmit={handleSubmit(onSubmit)} className="column">
+                        <Controller
+                            name="fName"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="fName"
+                                    label="First Name"
+                                    error={Boolean(errors.fName)}
+                                    helperText={errors.fName?.message}
+                                />}
+                        />
+                        <Controller
+                            name="lName"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="lName"
+                                    label="Last Name"
+                                    error={Boolean(errors.lName)}
+                                    helperText={errors.lName?.message}
+                                />}
+                        />
+                        <Controller
+                            name="username"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="username"
+                                    label="Username"
+                                    error={Boolean(errors.username)}
+                                    helperText={errors.username?.message}
+                                />}
+                        />
+                        <Controller
+                            name="email"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="email"
+                                    label="Email"
+                                    error={Boolean(errors.email)}
+                                    helperText={errors.email?.message}
+                                />}
+                        />
+                        <Controller
+                            name="password"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="password"
+                                    label="Password"
+                                    error={Boolean(errors.password)}
+                                    helperText={errors.password?.message}
+                                />}
+                        />
+                        <Controller
+                            name="confirmPassword"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    error={Boolean(errors.confirmPassword)}
+                                    helperText={errors.confirmPassword?.message}
+                                />}
+                        />
+                        <Button type="submit" variant="contained">Submit</Button>
                     </form>
-                    <p className="mt-5 mb-3 text-muted">&copy; Wi-Find | 2023</p>
-                </main>
-
+                </div>
                 <script type="module" src="./js/main.js"></script>
             </body>
-
-        </html>
+        </>
     );
 }
+

ffe017da63c0480df079d9dbb6eb421e60c6a12d
davidroygsu <124539273+davidroygsu@users.noreply.github.com>
2023-05-02 17:55:42 -0400

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
 dotenv.config()
 import Koa from 'koa';
 import bodyparser from 'koa-bodyparser';
@@ -18,58 +18,101 @@ var options = {
 app.use(cors(options));
 app.use(json());
 
-router.get('/record', async (ctx: any, next: any) => {
-    const db_connect = dbo.getDb("users");
-    const result = await db_connect.collection("records").find({}).toArray();
+// Retrives all users in the database
+router.get('/user', async (ctx: any, next: any) => {
+    const db_connect = dbo.getDb("propgo");
+    const result = await db_connect.collection("users").find({}).toArray();
     ctx.body = result;
-    next();
+    await next();
 });
 
-router.get('/record/:id', async (ctx: any, next: any) => {
+// Retrieves a user by their ID
+router.get('/user/:id', async (ctx: any, next: any) => {
     const db_connect = dbo.getDb();
     const myquery = { _id: new ObjectId(ctx.params.id) };
-    ctx.body = await db_connect.collection("records").findOne(myquery);
+    ctx.body = await db_connect.collection("users").findOne(myquery);
     console.log("This" + ctx.status);
     await next();
 });
 
-router.post('/record/add', async (ctx: any, next: any) => {
+// Adds a new user to the database
+router.post('/user/add', async (ctx: any, next: any) => {
     const db_connect = dbo.getDb();
     const myobj = {
-        username: ctx.request.body.username,
         fName: ctx.request.body.fName,
         lName: ctx.request.body.lName,
+        username: ctx.request.body.username,
         email: ctx.request.body.email,
         password: ctx.request.body.password,
         userType: ctx.request.body.userType,
     };
-    const insertOne = db_connect.collection("records").insertOne(myobj);
+    const insertOne = db_connect.collection("users").insertOne(myobj);
     ctx.body = insertOne;
 });
 
+// Updates a user by their ID
 router.post('/update/:id', async (ctx: any, next: any) => {
     const db_connect = dbo.getDb();
     const myquery = { _id: new ObjectId(ctx.params.id) };
     const newvalues = {
         $set: {
-            username: ctx.request.body.username,
             fName: ctx.request.body.fName,
             lName: ctx.request.body.lName,
+            username: ctx.request.body.username,
             email: ctx.request.body.email,
             password: ctx.request.body.password,
             userType: ctx.request.body.userType,
         },
     };
-    const updateOne = await db_connect.collection("records").updateOne(myquery, newvalues);
+    const updateOne = await db_connect.collection("users").updateOne(myquery, newvalues);
     console.log("1 document updated");
     ctx.body = updateOne;
 });
 
+// Deletes a user by their ID
 router.delete('/:id', async (ctx: any, next: any) => {
     const db_connect = dbo.getDb();
     const myquery = { _id: new ObjectId(ctx.params.id) };
-    const deleteOne = await db_connect.collection("records").deleteOne(myquery);
+    const deleteOne = await db_connect.collection("users").deleteOne(myquery);
     ctx.body = deleteOne;
+});
+
+// Authenticates a buyer user
+router.post('/buyerlogin', async (ctx: any, next: any) => {
+    const db_connect = dbo.getDb();
+    const { email, password } = ctx.request.body;
+    const user = await db_connect.collection("users").findOne({ email });
+    if (!user || user.password !== password) {
+        ctx.status = 401;
+        ctx.body = { message: 'Invalid email or password' }
+        return;
+    }
+    if (user.userType === 'seller') {
+        ctx.status = 401;
+        ctx.body = { message: 'Invalid user type for this page' };
+        console.log(ctx.body);
+        return;
+    }
+    ctx.body = { message: 'User logged in successfully' };
+});
+
+// Authenticates a seller user
+router.post('/sellerlogin', async (ctx: any, next: any) => {
+    const db_connect = dbo.getDb();
+    const { email, password, userType } = ctx.request.body;
+    const user = await db_connect.collection("users").findOne({ email });
+    if (!user || user.password !== password) {
+        ctx.status = 401;
+        ctx.body = { message: 'Invalid email or password' }
+        return;
+    }
+    if (user.userType === 'buyer') {
+        ctx.status = 401;
+        ctx.body = { message: 'Invalid user type for this page' };
+        console.log(ctx.body);
+        return;
+    }
+    ctx.body = { message: 'User logged in successfully' };
 });
 
 app

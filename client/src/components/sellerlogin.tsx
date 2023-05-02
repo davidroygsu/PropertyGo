e1cbb0ca162761d1085b3abffcb7a16488f88f37
davidroygsu <124539273+davidroygsu@users.noreply.github.com>
2023-05-02 00:32:15 -0400

    Update sellerlogin.tsx

--- client/src/components/sellerlogin.tsx
+++ client/src/components/sellerlogin.tsx
@@ -1,58 +1,89 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { object, string } from "yup";
+import { TextField, Button } from "@mui/material";
+import { ReactSession } from 'react-client-session';
 // Import our custom CSS
 import '../scss/styles.scss';
 
 export default function SellerLogin() {
+
+    let userSchema = object({
+        email: string().required('Email is required.').email('Must be a valid email.'),
+        password: string().required('Password is required.'),
+        userType: string().required(),
+    });
+
+    const { register, handleSubmit, formState: { errors }, reset, watch, control } = useForm({
+        resolver: yupResolver(userSchema),
+        defaultValues: { email: "", password: "", userType: "seller" }
+    });
+    const navigate = useNavigate();
+    async function onSubmit(e: any) {
+        const user = { ...e };
+
+        try {
+            const response = await fetch("http://localhost:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                body: JSON.stringify(user),
+            });
+
+            if (!response.ok) {
+                throw new Error('Credentials do not match');
+            }
+
+            ReactSession.set("userType", e.userType);
+            console.log(ReactSession.get("userType"));
+            reset();
+            navigate("/");
+        } catch (error) {
+            window.alert(error);
+            return;
+        }
+    }
     return (
-        <html lang="en">
-            <head>
-                <title>Wi-Find Seller | Login</title>
-            </head>
-
-            <body className="text-center">
-                <main className="form-signin w-100 m-auto">
-                    <form>
-                        <img className="mb-4" src="https://github.com/davidroygsu/SEstuff/blob/main/wifiangel.png?raw=true" alt="" width="72" height="57" />
-                        <h1 className="h3 mb-3 fw-normal">Wi-Find | Login</h1>
-
-                        <div className="row mb-3">
-                            <label htmlFor="sellerEmail" className="col-sm-3 col-form-label">Email</label>
-                            <div className="col-sm-9">
-                                <input type="email"
-                                    className="form-control"
-                                    id="sellerEmail" />
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <label htmlFor="sellerPassword" className="col-sm-3 col-form-label">Password</label>
-                            <div className="col-sm-9">
-                                <input type="password" className="form-control" id="sellerPassword" />
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <a href="sellerforgotpassword.html"> Forgot Password</a>
-                            </div>
-                            <div className="col-sm-6">
-                                <a href="/sellerregister"> Not yet registered?</a>
-                            </div>
-                        </div>
-
-                        <div className="row mb-3">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                        <p className="mt-5 mb-3 text-muted">&copy; Wi-Find | 2023</p>
+        <>
+            <body className="text-center calm">
+                <div>
+                    <h1>Seller Login</h1>
+                    <form onSubmit={handleSubmit(onSubmit)} className="column">
+                        <Controller
+                            name="email"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="email"
+                                    label="Email"
+                                    error={Boolean(errors.email)}
+                                    helperText={errors.email?.message}
+                                />}
+                        />
+                        <Controller
+                            name="password"
+                            control={control}
+                            render={({ field }) =>
+                                <TextField
+                                    {...field}
+                                    name="password"
+                                    label="Password"
+                                    error={Boolean(errors.password)}
+                                    helperText={errors.password?.message}
+                                />}
+                        />
+                        <Button type="submit" variant="contained">Submit</Button>
                     </form>
-                </main>
-
+                </div>
                 <script type="module" src="./js/main.js"></script>
             </body>
-
-        </html>
+        </>
     );
 }
